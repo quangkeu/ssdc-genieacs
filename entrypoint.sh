34a25899d85be101339e9645961a7063140f25b3
quangkeu <nlequang1@gmail.com>
2017-08-04 17:30:42 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ service redis-server start
 service rabbitmq-server start
 #rabbitmqadmin -q import /opt/ssdc-genieacs/rabbit.config
 ./opt/ssdc-genieacs/deploy_genieacs.sh
-./home/ubuntu/execute_on_qa.sh webapp $WEBAPP_NAME
+./home/ubuntu/execute_on_qa.sh webapp 
 sleep 30
-./home/ubuntu/execute_on_qa.sh backend $BACKEND_NAME
+./home/ubuntu/execute_on_qa.sh backend 
 while true; do sleep 1000; done
