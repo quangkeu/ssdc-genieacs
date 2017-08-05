04002c58b4561a71bc7fd9a18a0a773901699c7d
quangkeu <nlequang1@gmail.com>
2017-08-05 10:28:26 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-service mongod start
-service mysql start
-service redis-server start
-service rabbitmq-server start
+service mongod restart
+service mysql restart
+service redis-server restart
+service rabbitmq-server restart
 ./opt/ssdc-genieacs/deploy_genieacs.sh
+sleep 5
 ./home/ubuntu/execute_on_qa.sh webapp 
 sleep 30
 ./home/ubuntu/execute_on_qa.sh backend 
