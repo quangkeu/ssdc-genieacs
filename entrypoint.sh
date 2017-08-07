909711962e2a3cfbcfe9d505736a86e7ac795a8b
quangkeu <nlequang1@gmail.com>
2017-08-07 08:37:45 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 service mongod restart
 service mysql restart
+sleep 10
+mysql -uroot -proot -e "CREATE USER 'root'@'%' IDENTIFIED BY 'root';" 
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';" 
+mysql -uroot -proot -e "FLUSH PRIVILEGES;"
 service redis-server restart
 service rabbitmq-server restart
+sleep 10
+rabbitmqadmin -q import /opt/ssdc-genieacs/rabbit.config
 ./opt/ssdc-genieacs/deploy_genieacs.sh
 sleep 5
 ./home/ubuntu/execute_on_qa.sh webapp 
