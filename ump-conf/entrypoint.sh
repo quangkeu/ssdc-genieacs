75c5af6cf20cc730ae246db9c3810eb4f25673f9
quangkeu <nlequang1@gmail.com>
2017-08-08 13:27:20 +0700

    Update entrypoint.sh

--- ump-conf/entrypoint.sh
+++ ump-conf/entrypoint.sh
@@ -8,10 +8,10 @@ mysql -uroot -proot -e "FLUSH PRIVILEGES;"
 service redis-server restart
 service rabbitmq-server restart
 sleep 5
-rabbitmqadmin -q import /opt/ssdc-genieacs/rabbit.config
-./opt/ssdc-genieacs/deploy_genieacs.sh
+rabbitmqadmin -q import /home/ubuntu/ump-conf/rabbit.config
+./home/ubuntu/ump-conf/deploy_genieacs.sh
 sleep 5
-./home/ubuntu/execute_on_qa.sh webapp 
+./home/ubuntu/ump-conf/execute_on_qa.sh webapp 
 sleep 10
-./home/ubuntu/execute_on_qa.sh backend 
+/.home/ubuntu/ump-conf/execute_on_qa.sh backend 
 while true; do sleep 1000; done
